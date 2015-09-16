0f583f93dd5a99c545be8389df25bb6a41e4ef51
markhao <mark.q.hao@gmail.com>
2015-09-16 14:32:29 -0700

    Initial Commit

---
+++ iVulcun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iVulcun
+//
+//  Created by Yu Qi Hao on 9/16/15.
+//  Copyright (c) 2015 Yu Qi Hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iVulcun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iVulcun
+//
+//  Created by Yu Qi Hao on 9/16/15.
+//  Copyright (c) 2015 Yu Qi Hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
